8de9b2ae8d17ca5e5b116e6f799619fdb8bdcf55
Ivan Pashchenko <ivan.pashchenko@jetbrains.com>
2018-07-16 18:57:11 +0300

    fix not stopping containers

--- windowsservercore-ltsc2016/debug.cmd
+++ windowsservercore-ltsc2016/debug.cmd
@@ -1,4 +1,4 @@
-for /F %%A in ('docker ps -a -q ^| find "rider-windows"') do docker rm %%A
+for /F %%A in ('docker ps -a ^| find "rider-windows"') do docker rm %%A
 docker build -t rider-windows .
 docker run -dit --name rider-windows rider-windows
 docker attach rider-windows
