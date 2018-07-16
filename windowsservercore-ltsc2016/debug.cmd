9954b2929a5156e2f15546607356fd36f962358d
Ivan Pashchenko <ivan.pashchenko@jetbrains.com>
2018-07-16 18:57:51 +0300

    2Gb for images

--- windowsservercore-ltsc2016/debug.cmd
+++ windowsservercore-ltsc2016/debug.cmd
@@ -1,4 +1,4 @@
 for /F %%A in ('docker ps -a ^| find "rider-windows"') do docker rm %%A
-docker build -t rider-windows .
-docker run -dit --name rider-windows rider-windows
+docker build -t rider-windows -m 2Gb .
+docker run -dit --name rider-windows -m 2Gb rider-windows
 docker attach rider-windows
